7ce740d8daa263991947413fc2f330d8353f1079
joyce92200 <jowork2024@gmail.com>
2026-04-10 07:01:12 +0200

    Modify column types and add bulk insert statements
    Changed data types of several columns from DATE to NVARCHAR(50) and added bulk insert statements for multiple tables.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -84,9 +84,9 @@ CREATE TABLE bronze.crm_sales_details (
     sls_ord_num NVARCHAR(50),
     sls_prd_key NVARCHAR(50),
     sls_cust_id INT,
-    sls_order_dt DATE,   
-    sls_ship_dt DATE,    
-    sls_due_dt DATE,     
+    sls_order_dt NVARCHAR(50),   
+    sls_ship_dt NVARCHAR(50),    
+    sls_due_dt NVARCHAR(50),     
     sls_sales INT,
     sls_quantity INT,
     sls_price INT
@@ -96,7 +96,7 @@ IF OBJECT_ID('bronze.erp_CUST_AZ12', 'U') IS NOT NULL
    DROP TABLE bronze.erp_cust_az12;
 CREATE TABLE bronze.erp_cust_az12 (
     CID NVARCHAR(50),
-    BDATE DATE,
+    BDATE NVARCHAR(50),
     GEN NVARCHAR(10)
 );
 
@@ -115,5 +115,60 @@ CREATE TABLE bronze.erp_px_cat_g1v2 (
     ID NVARCHAR(50),
     CAT NVARCHAR(50),
     SUBCAT NVARCHAR(50),
-    MAINTENANCE BIT
+    MAINTENANCE NVARCHAR(50)
+);
+
+
+TRUNCATE TABLE bronze.crm_cust_info
+BULK INSERT bronze.crm_cust_info
+FROM 'C:\Users\jowor\OneDrive\Documents\1_Project\Baraa\source_crm\cust_info.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+TRUNCATE TABLE bronze.crm_prd_info -- refresh
+BULK INSERT bronze.crm_prd_info
+FROM 'C:\Users\jowor\OneDrive\Documents\1_Project\Baraa\source_crm\prd_info.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+TRUNCATE TABLE bronze.crm_sales_details -- refresh
+BULK INSERT bronze.crm_sales_details
+FROM 'C:\Users\jowor\OneDrive\Documents\1_Project\Baraa\source_crm\sales_details.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+TRUNCATE TABLE bronze.erp_cust_az12 -- refresh
+BULK INSERT bronze.erp_cust_az12
+FROM 'C:\Users\jowor\OneDrive\Documents\1_Project\Baraa\source_erp\CUST_AZ12.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+TRUNCATE TABLE bronze.erp_loc_a101 -- refresh
+BULK INSERT bronze.erp_loc_a101
+FROM 'C:\Users\jowor\OneDrive\Documents\1_Project\Baraa\source_erp\LOC_A101.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+TRUNCATE TABLE bronze.erp_px_cat_g1v2 -- refresh
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM 'C:\Users\jowor\OneDrive\Documents\1_Project\Baraa\source_erp\PX_CAT_G1V2.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
 );
