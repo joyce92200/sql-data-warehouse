bb14699bbaa26689261569c417282bd47968c77f
joyce92200 <jowork2024@gmail.com>
2026-04-10 10:59:17 +0200

    Enhance ddl_silver.sql with data cleansing queries
    Added data cleansing and quality check queries for customer information.

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -71,3 +71,48 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
     MAINTENANCE NVARCHAR(50),
     dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
+
+
+-- Check for NULLs or Duplicates in Primary Key
+-- Expectation : No Result
+
+SELECT
+cst_id,
+COUNT(*)
+FROM bronze.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL
+
+-- data cleansing
+SELECT 
+cst_id,
+cst_key,
+TRIM(cst_firstname) AS cst_firstname_clean,
+TRIM(cst_lastname) AS cst_lastname_clean,
+cst_marital_status,
+   CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+   WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married' 
+   ELSE 'n/a' 
+END AS cst_marital_status_clean,
+cst_gndr,
+   CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+   WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male' 
+   ELSE 'n/a' 
+END AS cst_gndr_clean,
+cst_create_date
+FROM (
+   SELECT 
+   *, 
+   ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last-- assigns a unique number to each row in a result set, based on a defined order, the latest date being on the top DESC
+   ) as temp
+WHERE flag_last = 1 AND cst_id = 29466 -- to check one by one
+
+-- quality check (unwanted spaces)
+-- Expectation : No result
+SELECT cst_firstname
+FROM bronze.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname)
+
+-- Data standarization & consistency (low cardinality) 
+SELECT DISTINCT cst_gndr
+FROM bronze.crm_cust_info
