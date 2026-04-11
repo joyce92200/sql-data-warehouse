93b26cc0c825a923a84cd5c8953dc1549efeabf5
joyce92200 <jowork2024@gmail.com>
2026-04-11 16:20:57 +0200

    Fix SQL syntax and improve data cleansing logic
    Refactor SQL script for data cleansing and table creation.

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -57,7 +57,7 @@ IF OBJECT_ID('silver.erp_LOC_A101', 'U') IS NOT NULL
 
 CREATE TABLE silver.erp_loc_a101 (
     CID NVARCHAR(50),
-    CNTRY NVARCHAR(50).
+    CNTRY NVARCHAR(50),
     dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
@@ -73,9 +73,8 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
 );
 
 
--- Check for NULLs or Duplicates in Primary Key
+-- Quality Check : Check for NULLs or Duplicates in Primary Key
 -- Expectation : No Result
-
 SELECT
 cst_id,
 COUNT(*)
@@ -83,36 +82,36 @@ FROM bronze.crm_cust_info
 GROUP BY cst_id
 HAVING COUNT(*) > 1 OR cst_id IS NULL
 
+
 -- data cleansing
+INSERT INTO silver.crm_cust_info(
+   cst_id,
+   cst_key,
+   cst_firstname,
+   cst_lastname,
+   cst_marital_status,
+   cst_gndr,
+   cst_create_date)
+   
 SELECT 
 cst_id,
 cst_key,
 TRIM(cst_firstname) AS cst_firstname_clean,
 TRIM(cst_lastname) AS cst_lastname_clean,
-cst_marital_status,
-   CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
-   WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married' 
-   ELSE 'n/a' 
+CASE 
+    WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+    WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married' 
+    ELSE 'n/a' 
 END AS cst_marital_status_clean,
-cst_gndr,
-   CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
-   WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male' 
-   ELSE 'n/a' 
+CASE
+    WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+    WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male' 
+    ELSE 'n/a' 
 END AS cst_gndr_clean,
 cst_create_date
 FROM (
-   SELECT 
-   *, 
-   ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last-- assigns a unique number to each row in a result set, based on a defined order, the latest date being on the top DESC
-   ) as temp
-WHERE flag_last = 1 AND cst_id = 29466 -- to check one by one
-
--- quality check (unwanted spaces)
--- Expectation : No result
-SELECT cst_firstname
-FROM bronze.crm_cust_info
-WHERE cst_firstname != TRIM(cst_firstname)
-
--- Data standarization & consistency (low cardinality) 
-SELECT DISTINCT cst_gndr
-FROM bronze.crm_cust_info
+   SELECT *, 
+   ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+   FROM bronze.crm_cust_info
+   ) AS temp
+WHERE flag_last = 1
