06737cfdbdb651ab2edff5b0bcb2fcfa6bf0551d
joyce92200 <jowork2024@gmail.com>
2026-04-12 16:28:41 +0200

    Add header comment to quality_checks_silver.sql
    Added a detailed comment header to the quality checks script, outlining its purpose and usage notes.

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -1,1 +1,19 @@
-==== quality check
+/*
+============
+Quality Checks
+==============
+Script Purpose :
+  This script performs various quality checks for data consistency, accuracy and standarization across the 'silver' schema. 
+  It includes checks for : 
+  - Null or duplicate primary keys
+  - unwanted spaces in string fields
+  - data standarization and consistency
+  - invalid data ranges and orders
+  - data consistency bewteen related fields
+
+Usage Notes:
+  - Run these checks after data loading Silver Layer
+  - Investigate and resolve any discrepencies found during the checks
+========
+*/
+
