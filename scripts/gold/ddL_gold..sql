4b491da5e8c9649a88b55e6719c04308d68e1b24
joyce92200 <jowork2024@gmail.com>
2026-04-15 06:48:23 +0200

    Create Gold layer views for customers, products, and sales
    This script creates views for the Gold layer in the data warehouse, replacing tables with views for analytics and reporting. It includes the creation of dimension views for customers and products, as well as a fact view for sales.

--- scripts/gold/ddL_gold..sql
+++ scripts/gold/ddL_gold..sql
@@ -1,5 +1,24 @@
--- Gold layer : use View instead of Tables (no stored procedures)
+/*
+================
+DDL Script: Create Gold Views
+================
+Script Purpose : 
+	This script creates views for the Gold layer in the data warehouse.
+	The Gold layer represents the final dimension and fact table (Star schema)
+
+	Each view performs transformations and combines data from the Silver layer
+	to produce a clean, enriched, and business-ready dataset.
+
+Usage : 
+	- These views can be queired directly for analytics and reporting
+*/
+
+
 -- create dimension customer
+
+IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
+	DROP VIEW gold.dim_customers;
+GO
 
 SELECT
 ci.cst_id,
@@ -44,7 +63,10 @@ SELECT cst_id, COUNT(*) FROM (
 	
 -- data integration for same columns in different tables - Surrogate Keys, SELECT DISTINCT, CASE WHEN, COALESCE
 -- Gold layer : use View instead of Tables (no stored procedures)
-
+IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
+	DROP VIEW gold.dim_customers;
+GO
+	
 CREATE VIEW gold.dim_customers AS 
 SELECT
 ROW_NUMBER() OVER(ORDER BY cst_id) AS customer_key, -- surrogate keys - system-generated unique identifier assigned to each record in a table
@@ -88,6 +110,10 @@ GROUP BY prd_key
 HAVING COUNT(*) >1
 
 -- Create View Dimension Products
+IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
+	DROP VIEW gold.dim_products;
+GO
+
 CREATE VIEW gold.dim_products AS
 SELECT
 	ROW_NUMBER() OVER(ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
@@ -105,3 +131,26 @@ FROM silver.crm_prd_info AS pn
 LEFT JOIN silver.erp_px_cat_g1v2 AS pc
 ON pn.category_id = pc.id
 WHERE prd_end_dt IS NULL
+
+--- Building Fact (Data Lookup)- use the dimension's surrogate keys instead of IDs to easily connect facts with dimensions
+IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
+	DROP VIEW gold.fact_sales;
+GO
+
+CREATE VIEW gold.fact_sales AS
+
+SELECT 	
+sd.sls_ord_num AS order_number,
+pr.product_key,
+cu.customer_key,
+sd.sls_order_dt AS order_date,
+sd.sls_ship_dt AS shipping_date,
+sd.sls_due_dt AS due_date,
+sd.sls_sales AS sales_amount,
+sd.sls_quantity AS quantity,
+sd.sls_price AS price
+FROM silver.crm_sales_details AS sd
+LEFT JOIN gold.dim_products AS pr
+ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers AS cu
+ON sd.sls_cust_id = cu.customer_id
